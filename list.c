35a3409091eee0127ee4de1e0448388eaa698e2e
Your Name <you@example.com>
2026-03-26 19:23:54 +0000

    new try

--- list.c
+++ list.c
@@ -89,7 +89,7 @@ void * popBack(List * list) {
 // Nota: El current debe quedar apuntando al nodo siguiente del eliminado.
 
 void * popCurrent(List * list) {
-    if(list->current != NULL && list->current->next != NULL) {
+    if(list->current != NULL && list->current->next != NULL && list->current->prev != NULL) {
         list->current->prev->next = list->current->next;
         list->current->next->prev = list->current->prev;
         void* dato = list->current->data;
